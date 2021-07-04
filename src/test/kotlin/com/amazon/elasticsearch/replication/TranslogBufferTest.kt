db307475d64b7402e0ce735925ff0c48fa0de83b
Rushi Agrawal <agrrushi@amazon.com>
2021-07-05 04:37:20 +0530

    add unit tests for translogbuffer class

--- src/test/kotlin/com/amazon/elasticsearch/replication/TranslogBufferTest.kt
+++ src/test/kotlin/com/amazon/elasticsearch/replication/TranslogBufferTest.kt
@@ -1,35 +1,87 @@
+/*
+ *   Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file. This file is distributed
+ *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *   express or implied. See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+
 package com.amazon.elasticsearch.replication
 
+import org.elasticsearch.test.ESTestCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
 
-internal class TranslogBufferTest {
+@ObsoleteCoroutinesApi
+class TranslogBufferTests : ESTestCase() {
 
-    fun setUp() {
-    }
+    @ExperimentalCoroutinesApi
+    fun `test translog buffer`() = runBlockingTest {
+        val tlb = TranslogBuffer(100)
 
-    fun tearDown() {
-    }
+        // perform first fetch and finish the first fetch
+        val estimate = tlb.getBatchSizeEstimateOrLockIfFirstFetch("idx1")
+        assert(estimate == tlb.FIRST_FETCH)
+        tlb.addEstimateAndUnlock("idx1", 30)
 
-    fun getTranslogBufferMutex() {
-    }
+        // perform next 'normal' fetch by occupying buffer
+        var (ok, inactive) = tlb.addBatch("idx1")
+        assertEquals(true, ok)
+        assertEquals(false, inactive)
 
-    fun setTranslogBufferMutex() {
-    }
+        // try to occupy more memory than what the buffer can hold
+        // Doesn't look like there's a way to destructure to same variables :(
+        val (ok2, inactive2) = tlb.addBatch("idx1")
+        assertEquals(true, ok2)
+        assertEquals(false, inactive2)
+        val (ok3, inactive3) = tlb.addBatch("idx1")
+        assertEquals(true, ok3)
+        assertEquals(false, inactive3)
+        val (ok4, inactive4) = tlb.addBatch("idx1")
+        assertEquals(false, ok4)
+        assertEquals(false, inactive4)
 
-    fun getBufferInitialSize() {
-    }
+        // replenishing buffer back again should succeed for 3 batches, and post that should fail
+        ok = tlb.removeBatch("idx1", false, false)
+        assertEquals(true, ok)
+        ok = tlb.removeBatch("idx1", false, false)
+        assertEquals(true, ok)
+        ok = tlb.removeBatch("idx1", false, false)
+        assertEquals(true, ok)
+        ok = tlb.removeBatch("idx1", false, false)
+        assertEquals(false, ok)
 
-    fun getBatchSizeEstimateOrLockIfFirstFetch() {
-    }
+        // trying to remove when buffer is completely empty should fail
+        ok = tlb.removeBatch("idx1", false, false)
+        assertEquals(false, ok)
 
-    fun addEstimateAndUnlock() {
-    }
+        // trying to remove when buffer is completely empty should fail, even if we're making index to become
+        // inactive at the moment
+        ok = tlb.removeBatch("idx1", true, false)
+        assertEquals(false, ok)
 
-    fun removeBatch() {
-    }
+        // marking an index inactive should allow for removing batches without memory constraint,
+        // now that the previous state was also inactive when we added this batch
+        ok = tlb.removeBatch("idx1", true, true)
+        assertEquals(true, ok)
 
-    fun addBatch() {
-    }
-
-    fun unlockIfLocked() {
+        // Adding batches to inactive index should work without memory constraint
+        tlb.addBatch("idx1")
+        tlb.addBatch("idx1")
+        tlb.addBatch("idx1")
+        tlb.addBatch("idx1")
+        tlb.addBatch("idx1")
+        val (ok5, inactive5) = tlb.addBatch("idx1")
+        assertEquals(true, ok5)
+        assertEquals(true, inactive5)
     }
 }
