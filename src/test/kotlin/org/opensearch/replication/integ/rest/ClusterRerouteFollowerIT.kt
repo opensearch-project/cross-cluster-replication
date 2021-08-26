3b3a7485b8d6f2d9d3c483b9bd7289e1fd20121a
Bhanu Tadepalli <tbhanu@amazon.com>
2021-08-26 11:02:28 +0530

    OS - Integ tests for reroutes

---
+++ src/test/kotlin/org/opensearch/replication/integ/rest/ClusterRerouteFollowerIT.kt
@@ -0,0 +1,84 @@
+package org.opensearch.replication.integ.rest
+
+import org.opensearch.replication.MultiClusterRestTestCase
+import org.opensearch.replication.MultiClusterAnnotations
+import org.opensearch.replication.StartReplicationRequest
+import org.opensearch.replication.startReplication
+import org.opensearch.replication.stopReplication
+import org.apache.http.entity.ContentType
+import org.apache.http.nio.entity.NStringEntity
+import org.assertj.core.api.Assertions
+import org.opensearch.client.Request
+import org.opensearch.client.RequestOptions
+import org.opensearch.client.indices.CreateIndexRequest
+import org.opensearch.client.indices.GetIndexRequest
+import org.opensearch.test.OpenSearchTestCase.assertBusy
+import java.util.concurrent.TimeUnit
+
+
+@MultiClusterAnnotations.ClusterConfigurations(
+        MultiClusterAnnotations.ClusterConfiguration(clusterName = LEADER),
+        MultiClusterAnnotations.ClusterConfiguration(clusterName = FOLLOWER)
+)
+
+class ClusterRerouteFollowerIT : MultiClusterRestTestCase() {
+    private val leaderIndexName = "leader_index"
+    private val followerIndexName = "follower_index"
+    fun `test replication works after rerouting a shard from one node to another in follower cluster`() {
+        val followerClient = getClientForCluster(FOLLOWER)
+        val leaderClient = getClientForCluster(LEADER)
+        try {
+            try {
+                changeTemplate(LEADER)
+            } catch (ex1 : Exception) {
+                logger.info("Changing template method is deprecated and throws an warning exception")
+            }
+            createConnectionBetweenClusters(FOLLOWER, LEADER)
+            val createIndexResponse = leaderClient.indices().create(CreateIndexRequest(leaderIndexName), RequestOptions.DEFAULT)
+            Assertions.assertThat(createIndexResponse.isAcknowledged).isTrue()
+            followerClient.startReplication(StartReplicationRequest("source", leaderIndexName, followerIndexName))
+            insertDocToIndex(LEADER, "1", "dummy data 1",leaderIndexName)
+
+            //Querying ES cluster throws random exceptions like MasterNotDiscovered or ShardsFailed etc, so catching them and retrying
+            assertBusy ({
+                try {
+                    Assertions.assertThat(docs(FOLLOWER, followerIndexName)).contains("dummy data 1")
+                } catch (ex: Exception) {
+                    Assertions.assertThat(true).isEqualTo(false)
+                }
+            }, 1, TimeUnit.MINUTES)
+
+            val nodes = getNodesInCluster(FOLLOWER)
+
+            val primaryNode = getPrimaryNodeForShard(FOLLOWER,followerIndexName, "0")
+            val unassignedNode = nodes.filter{!it.equals(primaryNode)}.stream().findFirst().get()
+            rerouteShard(FOLLOWER, "0", followerIndexName, primaryNode, unassignedNode)
+
+            assertBusy ({
+                Assertions.assertThat(getPrimaryNodeForShard(FOLLOWER,followerIndexName, "0")).isEqualTo(unassignedNode)
+            }, 1, TimeUnit.MINUTES)
+            logger.info("rereouted shard is " + getPrimaryNodeForShard(FOLLOWER,followerIndexName, "0"))
+            insertDocToIndex(LEADER, "2", "dummy data 2",leaderIndexName)
+
+            assertBusy ({
+                try {
+                    Assertions.assertThat(docs(FOLLOWER, followerIndexName)).contains("dummy data 2")
+                } catch (ex: Exception) {
+                    Assertions.assertThat(true).isEqualTo(false)
+                }
+            }, 1, TimeUnit.MINUTES)
+        } finally {
+            followerClient.stopReplication(followerIndexName)
+        }
+    }
+
+    private fun changeTemplate(clusterName: String) {
+        val cluster = getNamedCluster(clusterName)
+        val persistentConnectionRequest = Request("PUT", "_template/all")
+        val entityAsString = """
+                        {"template": "*", "settings": {"number_of_shards": 1, "number_of_replicas": 0}}""".trimMargin()
+
+        persistentConnectionRequest.entity = NStringEntity(entityAsString, ContentType.APPLICATION_JSON)
+        cluster.lowLevelClient.performRequest(persistentConnectionRequest)
+    }
+}
---
+++ src/test/kotlin/org/opensearch/replication/integ/rest/ClusterRerouteLeaderIT.kt
@@ -0,0 +1,85 @@
+package org.opensearch.replication.integ.rest
+
+import org.opensearch.replication.MultiClusterRestTestCase
+import org.opensearch.replication.MultiClusterAnnotations
+import org.opensearch.replication.StartReplicationRequest
+import org.opensearch.replication.startReplication
+import org.opensearch.replication.stopReplication
+import org.apache.http.entity.ContentType
+import org.apache.http.nio.entity.NStringEntity
+import org.assertj.core.api.Assertions
+import org.opensearch.client.Request
+import org.opensearch.client.RequestOptions
+import org.opensearch.client.indices.CreateIndexRequest
+import org.opensearch.client.indices.GetIndexRequest
+import org.opensearch.test.OpenSearchTestCase.assertBusy
+import org.junit.Assert
+import org.junit.Ignore
+import java.util.concurrent.TimeUnit
+
+@MultiClusterAnnotations.ClusterConfigurations(
+        MultiClusterAnnotations.ClusterConfiguration(clusterName = LEADER),
+        MultiClusterAnnotations.ClusterConfiguration(clusterName = FOLLOWER)
+)
+
+class ClusterRerouteLeaderIT : MultiClusterRestTestCase() {
+    private val leaderIndexName = "leader_index"
+    private val followerIndexName = "follower_index"
+
+    fun `test replication works after rerouting a shard from one node to another in leader cluster`() {
+        val followerClient = getClientForCluster(FOLLOWER)
+        val leaderClient = getClientForCluster(LEADER)
+        try {
+            try {
+                changeTemplate(LEADER)
+            } catch (ex1 : Exception) {
+                logger.info("Changing template method is deprecated and throws an warning exception")
+            }
+            createConnectionBetweenClusters(FOLLOWER, LEADER)
+            val createIndexResponse = leaderClient.indices().create(CreateIndexRequest(leaderIndexName), RequestOptions.DEFAULT)
+            Assertions.assertThat(createIndexResponse.isAcknowledged).isTrue()
+            followerClient.startReplication(StartReplicationRequest("source", leaderIndexName, followerIndexName))
+            insertDocToIndex(LEADER, "1", "dummy data 1",leaderIndexName)
+
+            //Querying ES cluster throws random exceptions like MasterNotDiscovered or ShardsFailed etc, so catching them and retrying
+            assertBusy ({
+                try {
+                    Assertions.assertThat(docs(FOLLOWER, followerIndexName)).contains("dummy data 1")
+                } catch (ex: Exception) {
+                    Assertions.assertThat(true).isEqualTo(false)
+                }
+            }, 1, TimeUnit.MINUTES)
+
+            val nodes = getNodesInCluster(LEADER)
+            val primaryNode = getPrimaryNodeForShard(LEADER,leaderIndexName, "0")
+            val unassignedNode = nodes.filter{!it.equals(primaryNode)}.stream().findFirst().get()
+            rerouteShard(LEADER, "0", leaderIndexName, primaryNode, unassignedNode)
+
+            assertBusy ({
+                Assertions.assertThat(getPrimaryNodeForShard(LEADER,leaderIndexName, "0")).isEqualTo(unassignedNode)
+            }, 1, TimeUnit.MINUTES)
+
+            insertDocToIndex(LEADER, "2", "dummy data 2",leaderIndexName)
+
+            assertBusy ({
+                try {
+                    Assertions.assertThat(docs(FOLLOWER, followerIndexName)).contains("dummy data 2")
+                } catch (ex: Exception) {
+                    Assertions.assertThat(true).isEqualTo(false)
+                }
+            }, 1, TimeUnit.MINUTES)
+        } finally {
+            followerClient.stopReplication(followerIndexName)
+        }
+    }
+
+    private fun changeTemplate(clusterName: String) {
+        val cluster = getNamedCluster(clusterName)
+        val persistentConnectionRequest = Request("PUT", "_template/all")
+        val entityAsString = """
+                        {"template": "*", "settings": {"number_of_shards": 1, "number_of_replicas": 0}}""".trimMargin()
+
+        persistentConnectionRequest.entity = NStringEntity(entityAsString, ContentType.APPLICATION_JSON)
+        cluster.lowLevelClient.performRequest(persistentConnectionRequest)
+    }
+}
