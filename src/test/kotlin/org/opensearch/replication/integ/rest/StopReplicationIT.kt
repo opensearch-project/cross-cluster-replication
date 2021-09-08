6b4aa90963c66e1cdb7946facc024ea0734becee
Ankit Kala <ankikala@amazon.com>
2021-09-08 18:19:06 +0530

    Uncomment StopReplicationITs

--- src/test/kotlin/org/opensearch/replication/integ/rest/StopReplicationIT.kt
+++ src/test/kotlin/org/opensearch/replication/integ/rest/StopReplicationIT.kt
@@ -83,7 +83,6 @@ class StopReplicationIT: MultiClusterRestTestCase() {
                                                     nFields: Int,
                                                     fieldLength: Int,
                                                     stepSize: Int) {
-        /*
         logger.info("""Testing stop replication in restoring state with params: 
             | shards:$settings[IndexMetadata.SETTING_NUMBER_OF_SHARDS]
             | nFields:$nFields
@@ -115,7 +114,6 @@ class StopReplicationIT: MultiClusterRestTestCase() {
                     .exists(GetIndexRequest(followerIndexName), RequestOptions.DEFAULT))
                     .isEqualTo(false)
         }
-         */
     }
 
     /* What we want to test here is the there is that STOP replication
