8fa868b6616d9fed18b6a88be677d6602f4c27a1
Monu Singh <msnghgw@amazon.com>
2022-10-04 14:53:57 +0530

    Support to send transport port as well
    Signed-off-by: Monu Singh <msnghgw@amazon.com>

--- scripts/integtest.sh
+++ scripts/integtest.sh
@@ -21,16 +21,11 @@ function usage() {
     echo -e "-t TRANSPORT_PORT\t, defaults to 9300, can be changed to any port for the cluster location."
     echo "--------------------------------------------------------------------------"
     echo "Multi cluster test:"
-    echo -e "-m Leader BIND_ADDRESS\t\t, defaults to localhost | 127.0.0.1, can be changed to any IP or domain name for the cluster location."
-    echo -e "-n Leader BIND_PORT\t\t, defaults to 9200, can be changed to any port for the cluster location."
-    echo -e "-o Leader TRANSPORT_PORT\t, defaults to 9300, can be changed to any port for the cluster location."
-    echo -e "-x Follower BIND_ADDRESS\t, defaults to localhost | 127.0.0.1, can be changed to any IP or domain name for the cluster location."
-    echo -e "-y Follower BIND_PORT\t\t, defaults to 9200, can be changed to any port for the cluster location."
-    echo -e "-z Follower TRANSPORT_PORT\t, defaults to 9300, can be changed to any port for the cluster location."
+    echo -e "-e Comma seperated endpoint:port, ex: localhost:9200,localhost:9201... ."
     echo "--------------------------------------------------------------------------"
 }
 
-while getopts ":h:b:p:t:m:n:o:x:y:z:s:c:" arg; do
+while getopts ":h:b:p:t:e:s:c:" arg; do
     case $arg in
         h)
             usage
@@ -45,23 +40,8 @@ while getopts ":h:b:p:t:m:n:o:x:y:z:s:c:" arg; do
          t)
             TRANSPORT_PORT=$OPTARG
             ;;
-        m)
-            LBIND_ADDRESS=$OPTARG
-            ;;
-        n)
-            LBIND_PORT=$OPTARG
-            ;;
-        o)
-            LTRANSPORT_PORT=$OPTARG
-            ;;
-        x)
-             FBIND_ADDRESS=$OPTARG
-            ;;
-        y)
-            FBIND_PORT=$OPTARG
-            ;;
-        z)
-            FTRANSPORT_PORT=$OPTARG
+        e)
+            ENDPOINT_LIST=$OPTARG
             ;;
         s)
             SECURITY_ENABLED=$OPTARG
@@ -102,48 +82,22 @@ PASSWORD=`echo $CREDENTIAL | awk -F ':' '{print $2}'`
 if [ -z "$BIND_ADDRESS" ] || [ -z "$BIND_PORT" ]
 then
   #Proceeding with multi cluster test
-  if [ -z "$LBIND_ADDRESS" ]
+  if [ -z "$ENDPOINT_LIST" ]
   then
-    echo "requires an argument -m <Leader bind address>"
+    echo "requires an argument -e <endpoint:port>"
     usage
     exit 1
   fi
 
-  if [ -z "$LBIND_PORT" ]
-  then
-    echo "requires an argument -n <Leader bind port>"
-    usage
-    exit 1
-  fi
+  leader=$(echo  $ENDPOINT_LIST | cut -d ',' -f1 | cut -d ':' -f1,2 )
+  follower=$(echo $ENDPOINT_LIST  |  cut -d ',' -f1 | cut -d ':' -f1,2 )
 
-  if [ -z "$LTRANSPORT_PORT" ]
-  then
-    echo "requires an argument -o <Leader transport port>"
-    usage
-    exit 1
-  fi
+  FTRANSPORT_PORT=$(echo  $ENDPOINT_LIST | cut -d ',' -f1 | cut -d ':' -f3  )
+  LTRANSPORT_PORT=$(echo  $ENDPOINT_LIST | cut -d ',' -f2 | cut -d ':' -f3  )
+  echo "./gradlew integTestRemote -Dleader.http_host=\"$leader\" -Dfollower.http_host=\"$follower\" -Dfollower.transport_host=\"$FTRANSPORT_PORT\"  -Dleader.transport_host=\"$LTRANSPORT_PORT\"  -Dsecurity_enabled=\"$SECURITY_ENABLED\" -Duser=\"$USERNAME\" -Dpassword=\"$PASSWORD\" --console=plain "
+  eval "./gradlew integTestRemote -Dleader.http_host=\"$leader\" -Dfollower.http_host=\"$follower\" -Dfollower.transport_host=\"$FTRANSPORT_PORT\"  -Dleader.transport_host=\"$LTRANSPORT_PORT\"  -Dsecurity_enabled=\"$SECURITY_ENABLED\" -Duser=\"$USERNAME\" -Dpassword=\"$PASSWORD\" --console=plain "
 
-  if [ -z "$FBIND_ADDRESS" ]
-  then
-    echo "requires an argument -fb <Follower bind address>"
-    usage
-    exit 1
-  fi
 
-  if [ -z "$FBIND_PORT" ]
-  then
-    echo "requires an argument -fp <Follower bind port>"
-    usage
-    exit 1
-  fi
-
-  if [ -z "$FTRANSPORT_PORT" ]
-  then
-    echo "requires an argument -ft <Follower transport port>"
-    usage
-    exit 1
-  fi
-  eval "./gradlew integTestRemote -Dleader.http_host=\"$LBIND_ADDRESS:$LBIND_PORT\" -Dleader.transport_host=\"$LBIND_ADDRESS:$LTRANSPORT_PORT\" -Dfollower.http_host=\"$FBIND_ADDRESS:$FBIND_PORT\" -Dfollower.transport_host=\"$FBIND_ADDRESS:$FTRANSPORT_PORT\" -Dsecurity_enabled=\"$SECURITY_ENABLED\" -Duser=\"$USERNAME\" -Dpassword=\"$PASSWORD\" --console=plain "
 
 else
   # Single cluster
