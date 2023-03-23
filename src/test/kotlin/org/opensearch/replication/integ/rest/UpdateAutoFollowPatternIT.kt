ac2040ab1a9ee011c1a9f32f86cd3b64db5e014a
Monu Singh <msnghgw@amazon.com>
2023-03-23 09:06:46 +0530

    Merge pull request #745 from opensearch-project/monu-aws-patch-1 Signed-off-by: Monu Singh <msnghgw@amazon.com> (cherry picked from commit f9746e54d030828a1ea0721456fe10dd094010cb)

--- src/test/kotlin/org/opensearch/replication/integ/rest/UpdateAutoFollowPatternIT.kt
+++ src/test/kotlin/org/opensearch/replication/integ/rest/UpdateAutoFollowPatternIT.kt
@@ -197,6 +197,8 @@ class UpdateAutoFollowPatternIT: MultiClusterRestTestCase() {
             }, 30, TimeUnit.SECONDS)
             // Verify that existing index matching the pattern are replicated.
             assertBusy ({
+                followerClient.waitForShardTaskStart(leaderIndexName)
+                followerClient.waitForShardTaskStart(leaderIndexName2)
                 Assertions.assertThat(followerClient.indices()
                         .exists(GetIndexRequest(leaderIndexName2), RequestOptions.DEFAULT))
                         .isEqualTo(true)
@@ -208,12 +210,10 @@ class UpdateAutoFollowPatternIT: MultiClusterRestTestCase() {
                     assert(key["num_success_start_replication"]!! as Int == 1)
                 }
                 assertTrue(af_stats.size == 2)
-            }, 30, TimeUnit.SECONDS)
+            }, 60, TimeUnit.SECONDS)
         } finally {
             followerClient.deleteAutoFollowPattern(connectionAlias, indexPatternName)
             followerClient.deleteAutoFollowPattern(connectionAlias, indexPatternName2)
-            followerClient.waitForShardTaskStart(leaderIndexName)
-            followerClient.waitForShardTaskStart(leaderIndexName2)
         }
     }
 
