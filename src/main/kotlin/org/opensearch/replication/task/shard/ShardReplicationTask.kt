4a5d16e0a8f47dae80caf7fed205f771ba85376e
sricharanvuppu <113983630+sricharanvuppu@users.noreply.github.com>
2023-06-28 10:32:00 +0530

    Handling OpenSearchRejectExecuteException Exception (#1004)
    * Handling OpenSearchRejectExecuteException Exception
    * introduced writersPerShard setting.
    
    Signed-off-by: sricharanvuppu <srivuppu@amazon.com>
    (cherry picked from commit 448e7a7501e5ff8740dc2d7635c08ae62d19147e)
    Signed-off-by: sricharanvuppu <srivuppu@amazon.com>

--- src/main/kotlin/org/opensearch/replication/task/shard/ShardReplicationTask.kt
+++ src/main/kotlin/org/opensearch/replication/task/shard/ShardReplicationTask.kt
@@ -214,7 +214,7 @@ class ShardReplicationTask(id: Long, type: String, action: String, description: 
         // Since this setting is not dynamic, setting update would only reflect after pause-resume or on a new replication job.
         val rateLimiter = Semaphore(replicationSettings.readersPerShard)
         val sequencer = TranslogSequencer(scope, replicationMetadata, followerShardId, leaderAlias, leaderShardId.indexName,
-                                          TaskId(clusterService.nodeName, id), client, indexShard.localCheckpoint, followerClusterStats)
+                                          TaskId(clusterService.nodeName, id), client, indexShard.localCheckpoint, followerClusterStats, replicationSettings.writersPerShard)
 
         val changeTracker = ShardReplicationChangesTracker(indexShard, replicationSettings)
         followerClusterStats.stats[followerShardId]!!.followerCheckpoint = indexShard.localCheckpoint
@@ -251,7 +251,6 @@ class ShardReplicationTask(id: Long, type: String, action: String, description: 
                         followerClusterStats.stats[followerShardId]!!.opsReadFailures.addAndGet(1)
                         logInfo("Unable to get changes from seqNo: $fromSeqNo. ${e.stackTraceToString()}")
                         changeTracker.updateBatchFetched(false, fromSeqNo, toSeqNo, fromSeqNo - 1,-1)
-
                         // Propagate 4xx exceptions up the chain and halt replication as they are irrecoverable
                         val range4xx = 400.rangeTo(499)
                         if (e is OpenSearchException &&
