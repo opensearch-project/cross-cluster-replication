5be126f36378c1d4a322fdd956cae5d39d41a96f
sricharanvuppu <113983630+sricharanvuppu@users.noreply.github.com>
2023-07-07 21:19:21 +0530

    ratelimiter acquiring after getting unAppliedChanges in TranslogSequncer
    Signed-off-by: sricharanvuppu <113983630+sricharanvuppu@users.noreply.github.com>

--- src/main/kotlin/com/amazon/elasticsearch/replication/task/shard/TranslogSequencer.kt
+++ src/main/kotlin/com/amazon/elasticsearch/replication/task/shard/TranslogSequencer.kt
@@ -75,12 +75,12 @@ class TranslogSequencer(scope: CoroutineScope, private val replicationMetadata: 
         val rateLimiter = Semaphore(writersPerShard)
         var highWatermark = initialSeqNo
         for (m in channel) {
-            rateLimiter.acquire()
             while (unAppliedChanges.containsKey(highWatermark + 1)) {
                 val next = unAppliedChanges.remove(highWatermark + 1)!!
                 val replayRequest = ReplayChangesRequest(followerShardId, next.changes, next.maxSeqNoOfUpdatesOrDeletes,
                                                          leaderAlias, leaderIndexName)
                 replayRequest.parentTask = parentTaskId
+                rateLimiter.acquire()
                 launch {
                     var relativeStartNanos  = System.nanoTime()
                     val retryOnExceptions = ArrayList<Class<*>>()
