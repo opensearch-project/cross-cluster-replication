9750b7800a208b9f2bea2646813dc59857cfed73
Monu Singh <msnghgw@amazon.com>
2023-08-22 18:55:50 +0530

    Revert "Add setting to use document replication for system indices. (#802)" (#1107)
    This reverts commit 55b6968af90d739d8448c5d88ed5204a240d0f86.
    
    Signed-off-by: monusingh-1 <msnghgw@amazon.com>

--- src/main/kotlin/org/opensearch/replication/metadata/store/ReplicationMetadataStore.kt
+++ src/main/kotlin/org/opensearch/replication/metadata/store/ReplicationMetadataStore.kt
@@ -40,7 +40,6 @@ import org.opensearch.common.xcontent.LoggingDeprecationHandler
 import org.opensearch.core.xcontent.NamedXContentRegistry
 import org.opensearch.core.xcontent.ToXContent
 import org.opensearch.core.xcontent.XContentParser
-import org.opensearch.indices.replication.common.ReplicationType
 import org.opensearch.replication.util.suspendExecuteWithRetries
 
 class ReplicationMetadataStore constructor(val client: Client, val clusterService: ClusterService,
@@ -266,7 +265,6 @@ class ReplicationMetadataStore constructor(val client: Client, val clusterServic
                 .put(IndexMetadata.INDEX_AUTO_EXPAND_REPLICAS_SETTING.key, "0-1")
                 .put(IndexMetadata.INDEX_PRIORITY_SETTING.key, Int.MAX_VALUE)
                 .put(IndexMetadata.INDEX_HIDDEN_SETTING.key, true)
-                .put(IndexMetadata.INDEX_REPLICATION_TYPE_SETTING.key, ReplicationType.DOCUMENT) // System Indices should use Document Replication strategy
                 .build()
     }
 
