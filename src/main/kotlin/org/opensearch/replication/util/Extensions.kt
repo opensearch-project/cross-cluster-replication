426a2deb85bc3c58a3439e829e6902e68e6f658d
Mohit Kumar <113413713+mohitamg@users.noreply.github.com>
2023-09-04 15:38:16 +0530

    Removed timeout excessive logging in case of index is idle in replication (#1114)
    * Removed timeout excessive logging in case of index is idle in replication
    
    Signed-off-by: Mohit Kumar <mohitamg@amazon.com>
    
    * Removed timeout excessive logging in case of index is idle in replication
    
    Signed-off-by: Mohit Kumar <mohitamg@amazon.com>
    
    * Removed timeout excessive stack trace logging in case of index is idle in replication
    
    Signed-off-by: Mohit Kumar <mohitamg@amazon.com>
    
    * Removed timeout excessive stack trace logging in case of index is idle in replication
    
    Signed-off-by: Mohit Kumar <mohitamg@amazon.com>
    
    * Changed the log statement
    
    Signed-off-by: Mohit Kumar <mohitamg@amazon.com>
    
    ---------
    
    Signed-off-by: Mohit Kumar <mohitamg@amazon.com>

--- src/main/kotlin/org/opensearch/replication/util/Extensions.kt
+++ src/main/kotlin/org/opensearch/replication/util/Extensions.kt
@@ -138,8 +138,8 @@ suspend fun <Req: ActionRequest, Resp: ActionResponse> Client.suspendExecuteWith
             }
         }
         log.warn(
-            "Encountered a failure while executing in $req. Retrying in ${currentBackoff / 1000} seconds" +
-                    ".", retryException
+            "Encountered a failure(can be ignored) while getting changes:  OpenSearchTimeoutException. Retrying in ${currentBackoff / 1000} seconds" +
+                    "."
         )
         delay(currentBackoff)
         currentBackoff = (currentBackoff * factor).toLong().coerceAtMost(maxTimeOut)
