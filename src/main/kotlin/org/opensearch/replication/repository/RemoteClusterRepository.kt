9ef2f4a55fe21465bdfa33c83e8551696e3a2628
monusingh-1 <msnghgw@amazon.com>
2023-09-07 14:50:49 +0530

    Implement abstract methods
    Signed-off-by: monusingh-1 <msnghgw@amazon.com>

--- src/main/kotlin/org/opensearch/replication/repository/RemoteClusterRepository.kt
+++ src/main/kotlin/org/opensearch/replication/repository/RemoteClusterRepository.kt
@@ -107,6 +107,14 @@ class RemoteClusterRepository(private val repositoryMetadata: RepositoryMetadata
         return restoreRateLimitingTimeInNanos.count()
     }
 
+    override fun getRemoteUploadThrottleTimeInNanos(): Long {
+        throw UnsupportedOperationException("Operation not permitted")
+    }
+
+    override fun getRemoteDownloadThrottleTimeInNanos(): Long {
+        throw UnsupportedOperationException("Operation not permitted")
+    }
+
     override fun finalizeSnapshot(shardGenerations: ShardGenerations?, repositoryStateId: Long, clusterMetadata: Metadata?,
                                   snapshotInfo: SnapshotInfo?, repositoryMetaVersion: Version?,
                                   stateTransformer: Function<ClusterState, ClusterState>?,
@@ -349,6 +357,10 @@ class RemoteClusterRepository(private val repositoryMetadata: RepositoryMetadata
 
     override fun isReadOnly(): Boolean {
         return true
+    }
+
+    override fun isSystemRepository(): Boolean {
+        throw UnsupportedOperationException("Operation not permitted")
     }
 
 
